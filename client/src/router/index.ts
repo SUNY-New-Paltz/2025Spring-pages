2e73fe6605ce0b458cb92b7dad0efb7a8fdafa53
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-24 12:02:51 -0400

    testing urls 2

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory('/2025Spring-pages/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
